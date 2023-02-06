f739e6b97741d1334db67258aff2384615b56198
Drfinik <freelanceap@yahoo.com>
2023-02-06 16:02:05 +0300

    Еxample 005 ConditionIfElse project

--- Еxample005_ConditionIfElse/Program.cs
+++ Еxample005_ConditionIfElse/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write ("Введите имя пользователя:"); 
+﻿/*Console.Write ("Введите имя пользователя:"); 
 string username = Console.ReadLine(); 
 if(username == "Маша") 
 { 
@@ -6,6 +6,18 @@ Console.WriteLine("Ура, это же Маша!");
 }
 else 
 { 
+Console.Write("Привет, ");  
+Console.WriteLine(username); 
+} */
+
+Console.Write ("Введите имя пользователя:"); 
+string username = Console.ReadLine (); 
+if(username.ToLower() == "Маша")
+{ 
+Console.WriteLine("Ура, это же МАША"); 
+}
+else
+{
 Console.Write("Привет, ");  
 Console.WriteLine(username); 
 } 
