841b72ea7124674d8eab6cdb19082fe4cd35f523
Drfinik <freelanceap@yahoo.com>
2023-02-06 14:49:19 +0300

    Еxample 005 ConditionIfElse project

--- Еxample003_Sum/Program.cs
+++ Еxample003_Sum/Program.cs
@@ -7,3 +7,15 @@ int numberВ = 5;
 int result = numberA + numberВ; 
 Console.WriteLine(result); */
 
+/*int numberA = new Random().Next(1,10); 
+int numberB = new Random().Next(1,10); 
+int result = numberA + numberB; 
+Console.WriteLine(result); */
+
+int numberA = new Random().Next(1,10); 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); 
+Console.WriteLine(numberB);
+int result = numberA + numberB; 
+Console.WriteLine(result);
+
---
+++ Еxample005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя:"); 
+string username = Console.ReadLine(); 
+if(username == "Маша") 
+{ 
+Сonsole.WriteLine(Ура, это же Маша!);
+}
+else 
+{ 
+Сonsole.Write("Привет, ");  
+Сonsole.WriteLine(username); 
+} 
