3f1944ebcd20bf888844b8c53cafa6e030f972bb
Drfinik <freelanceap@yahoo.com>
2023-02-08 20:18:33 +0300

    добавил gitignore

--- Еxample005_ConditionIfElse/Program.cs
+++ Еxample005_ConditionIfElse/Program.cs
@@ -10,11 +10,13 @@ Console.Write("Привет, ");
 Console.WriteLine(username); 
 } */
 
+
 Console.Write ("Введите имя пользователя:"); 
-string username = Console.ReadLine (); 
-if(username.ToLower() == "Маша")
+string username = Console.ReadLine(); 
+
+if(username.ToLower()=="Маша")
 { 
-Console.WriteLine("Ура, это же МАША"); 
+Console.WriteLine("Ура, это же МАША!"); 
 }
 else
 {
